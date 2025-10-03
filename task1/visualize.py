722ce8197566e10ebf0c3a6e3ebb47a2a8c7ba68
ogershony <ogershony@gmail.com>
2025-10-02 22:55:06 -0700

    completed task 1

--- task1/visualize.py
+++ task1/visualize.py
@@ -12,6 +12,7 @@ def main():
     # model = mujoco.MjModel.from_xml_path("meshes/lego_hand/lego_hand.xml")
     model = mujoco.MjModel.from_xml_path("../assets/descriptions/DropCubeInBinEnv.xml")
     data = mujoco.MjData(model)
+    #camera_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_CAMERA, "fixed")
     
     # Initialize with default XML values
     mujoco.mj_resetData(model, data)
@@ -23,9 +24,10 @@ def main():
     print(f"Actuators: {model.nu}")
     
     # Launch viewer
-    with mujoco.viewer.launch_passive(model, data) as viewer:
+    with mujoco.viewer.launch(model, data) as viewer:
         print("Viewer launched. Press ESC to exit.")
-        
+        #viewer.update_scene(data, camera_id)
+
         while viewer.is_running():
             # Step simulation
             mujoco.mj_step(model, data)
