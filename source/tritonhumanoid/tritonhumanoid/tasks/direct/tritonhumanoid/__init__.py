f6c95519aaa81d574f3ce1348aa5d21cffb90d76
DarinAnthony <darinanthonyad@gmail.com>
2025-11-16 10:32:16 +0000

    Initial commit

---
+++ source/tritonhumanoid/tritonhumanoid/tasks/direct/tritonhumanoid/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Tritonhumanoid-Direct-v0",
+    entry_point=f"{__name__}.tritonhumanoid_env:TritonhumanoidEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.tritonhumanoid_env_cfg:TritonhumanoidEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
