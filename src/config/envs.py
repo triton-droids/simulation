1f30c75e76e0a579b1095bf05a90a3f5687a1203
DarinAnthony <darinanthonyad@gmail.com>
2026-01-15 18:17:10 -0800

    test push

--- src/config/envs.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class HumanoidLegsEnv:
-    name: str = "default_humanoid_legs"
-    terrain: str = "flat"
--- src/config/robots.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class DefaultHumanoidLegsRobot:
-    name: str = "default_humanoid_legs"
-    
